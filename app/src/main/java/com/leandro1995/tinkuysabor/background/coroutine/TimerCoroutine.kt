c772e4f396ce62ea60c902340abf3879a8a30ef4
LEANDRO\leccb <Leandro>
2025-07-10 02:34:40 -0500

    Crear método de temporizador para la app

---
+++ app/src/main/java/com/leandro1995/tinkuysabor/background/coroutine/TimerCoroutine.kt
@@ -0,0 +1,29 @@
+package com.leandro1995.tinkuysabor.background.coroutine
+
+import com.leandro1995.tinkuysabor.background.config.TimeType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.CoroutineContext
+
+class TimerCoroutine(
+    private val dispatcher: CoroutineContext = Dispatchers.Main,
+    private val timeType: TimeType,
+    private val time: Long
+) {
+
+    fun timeStart(method: () -> Unit) {
+
+        CoroutineScope(dispatcher).launch {
+            timeType().sleep(time)
+            method()
+        }
+    }
+
+    private fun timeType() = when (timeType) {
+        TimeType.SECOND -> TimeUnit.SECONDS
+        TimeType.MINUTE -> TimeUnit.MINUTES
+        TimeType.HOUR -> TimeUnit.HOURS
+    }
+}
