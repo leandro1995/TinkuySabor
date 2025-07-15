59ed49b1dc87170be5f5d58e04664ef204364d31
LEANDRO\leccb <Leandro>
2025-07-14 19:17:09 -0500

    Agregar usuarios para el firebase con Google Login

--- app/src/main/java/com/leandro1995/tinkuysabor/fcm/authentication/FCMGoogleAuthentication.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/fcm/authentication/FCMGoogleAuthentication.kt
@@ -5,13 +5,11 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 
-class FCMGoogleAuthentication {
+class FCMGoogleAuthentication(private val googleToken: String) {
 
     private val authentication = Firebase.auth
 
-    fun registerUser(
-        googleToken: String, success: (currentUser: FirebaseUser) -> Unit, error: () -> Unit
-    ) {
+    fun registerUser(success: (currentUser: FirebaseUser) -> Unit, error: () -> Unit) {
         authentication.let { auth ->
             auth.signInWithCredential(GoogleAuthProvider.getCredential(googleToken, null))
                 .addOnCompleteListener { task ->
--- app/src/main/java/com/leandro1995/tinkuysabor/intent/action/LoginIntentAction.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/intent/action/LoginIntentAction.kt
@@ -1,9 +1,13 @@
 package com.leandro1995.tinkuysabor.intent.action
 
+import com.leandro1995.tinkuysabor.fcm.authentication.FCMGoogleAuthentication
 import com.leandro1995.tinkuysabor.fcm.login.FCMGoogleLogin
 
 sealed class LoginIntentAction {
     data object InitView : LoginIntentAction()
 
     data class GoogleLogin(val fcmGoogleLogin: FCMGoogleLogin) : LoginIntentAction()
+
+    data class GoogleAuthentication(val fcmGoogleAuthentication: FCMGoogleAuthentication) :
+        LoginIntentAction()
 }
