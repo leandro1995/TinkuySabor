39226321a869f7f67b3a1308b1ffcdc7c1578a02
LEANDRO\leccb <Leandro>
2025-07-15 00:20:29 -0500

    Eliminar el FirebaseUser del parámetro en la clase FCMGoogleAuthentication

--- app/src/main/java/com/leandro1995/tinkuysabor/fcm/authentication/FCMGoogleAuthentication.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/fcm/authentication/FCMGoogleAuthentication.kt
@@ -1,7 +1,6 @@
 package com.leandro1995.tinkuysabor.fcm.authentication
 
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 
@@ -9,13 +8,13 @@ class FCMGoogleAuthentication(private val googleToken: String) {
 
     private val authentication = Firebase.auth
 
-    fun registerUser(success: (currentUser: FirebaseUser) -> Unit, error: () -> Unit) {
+    fun registerUser(success: () -> Unit, error: () -> Unit) {
         authentication.let { auth ->
             auth.signInWithCredential(GoogleAuthProvider.getCredential(googleToken, null))
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         auth.currentUser?.let { currentUser ->
-                            success(currentUser)
+                            success()
                         }
                     } else {
                         error()
