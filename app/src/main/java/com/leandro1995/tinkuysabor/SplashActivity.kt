bc44d7b23af06f80745f390f69fef1057aee240d
LEANDRO\leccb <Leandro>
2025-07-16 22:46:43 -0500

    Crear e implementar SplashViewModel

--- app/src/main/java/com/leandro1995/tinkuysabor/SplashActivity.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/SplashActivity.kt
@@ -3,16 +3,19 @@ package com.leandro1995.tinkuysabor
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.leandro1995.tinkuysabor.activity.LoginActivity
 import com.leandro1995.tinkuysabor.background.config.TimeType
 import com.leandro1995.tinkuysabor.background.coroutine.TimerCoroutine
+import com.leandro1995.tinkuysabor.viewmodel.SplashViewModel
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private val timeCoroutine = TimerCoroutine(timeType = TimeType.SECOND, time = TIME_OUT)
+    private val splashViewModel by viewModels<SplashViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/leandro1995/tinkuysabor/viewmodel/SplashViewModel.kt
@@ -0,0 +1,7 @@
+package com.leandro1995.tinkuysabor.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+    
+}
