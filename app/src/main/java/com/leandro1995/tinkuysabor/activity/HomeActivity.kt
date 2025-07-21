d868bf4454bbb25b5a0bc2b1339d161c5dbd6b77
LEANDRO\leccb <Leandro>
2025-07-20 21:04:52 -0500

    Mostrar fragmentos del navegation en la app

--- app/src/main/java/com/leandro1995/tinkuysabor/activity/HomeActivity.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/activity/HomeActivity.kt
@@ -5,11 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.leandro1995.tinkuysabor.R
+import com.leandro1995.tinkuysabor.databinding.ActivityHomeBinding
+import com.leandro1995.tinkuysabor.extension.bindingUtil
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var activityHomeBinding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        activityHomeBinding = bindingUtil(layoutId = R.layout.activity_home)
+
+        initView()
+    }
+
+    private fun initView() {
+        (supportFragmentManager.findFragmentById(R.id.homeContentView) as NavHostFragment).navController.apply {
+            activityHomeBinding.homeNavigation.setupWithNavController(this)
+        }
     }
 }
