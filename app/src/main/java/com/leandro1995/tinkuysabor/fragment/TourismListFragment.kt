233c43c3dd3dc906c0f196ee833569aeaa2a26b1
LEANDRO\leccb <Leandro>
2025-07-31 18:59:59 -0500

    Implementar binding para el fragmento de TourismListFragment

--- app/src/main/java/com/leandro1995/tinkuysabor/fragment/TourismListFragment.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/fragment/TourismListFragment.kt
@@ -6,12 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.leandro1995.tinkuysabor.R
+import com.leandro1995.tinkuysabor.databinding.FragmentTourismListBinding
+import com.leandro1995.tinkuysabor.extension.bindingUtil
 
 class TourismListFragment : Fragment() {
+
+    private lateinit var fragmentTourismListBinding: FragmentTourismListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tourism_list, container, false)
+
+        fragmentTourismListBinding = bindingUtil(
+            layoutId = R.layout.fragment_tourism_list,
+            inflater = inflater,
+            container = container
+        )
+
+        return fragmentTourismListBinding.root
     }
 }
