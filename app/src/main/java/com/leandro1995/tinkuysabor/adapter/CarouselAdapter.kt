8ca4551d666332fd2314c97008dcdfb0e9787be0
LEANDRO\leccb <Leandro>
2025-07-30 20:20:40 -0500

    Actualizar adapter de carrusel  con binding

--- app/src/main/java/com/leandro1995/tinkuysabor/adapter/CarouselAdapter.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/adapter/CarouselAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.leandro1995.tinkuysabor.R
 import com.leandro1995.tinkuysabor.adapter.holder.CarouselHolder
+import com.leandro1995.tinkuysabor.databinding.ItemCarouselBinding
 import com.leandro1995.tinkuysabor.model.design.Carousel
 
 class CarouselAdapter(private val carouselArrayList: ArrayList<Carousel>) :
@@ -14,17 +15,22 @@ class CarouselAdapter(private val carouselArrayList: ArrayList<Carousel>) :
         parent: ViewGroup, viewType: Int
     ): CarouselHolder {
         return CarouselHolder(
-            view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_carousel, parent, false)
+            itemCarouselBinding = ItemCarouselBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
         )
     }
 
     override fun onBindViewHolder(
         holder: CarouselHolder, position: Int
     ) {
-        holder.titleText.text = carouselArrayList[position].title
-        holder.descriptionText.text = carouselArrayList[position].description
-        holder.carouselImage.setImageResource(carouselArrayList[position].imageDrawable)
+        holder.itemCarouselBinding.apply {
+            titleText.text = carouselArrayList[position].title
+            descriptionText.text = carouselArrayList[position].description
+            carouselImage.setImageResource(carouselArrayList[position].imageDrawable)
+        }
     }
 
     override fun getItemCount(): Int {
