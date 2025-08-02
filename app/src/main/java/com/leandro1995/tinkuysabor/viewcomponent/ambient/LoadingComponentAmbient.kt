9d13b6efc99e099566e7aea64dfdbc3f32dfcd8d
LEANDRO\leccb <Leandro>
2025-08-02 05:55:07 -0500

    Crear cargando parte visual del componente personalizado

--- app/src/main/java/com/leandro1995/tinkuysabor/viewcomponent/ambient/LoadingComponentAmbient.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/viewcomponent/ambient/LoadingComponentAmbient.kt
@@ -3,7 +3,7 @@ package com.leandro1995.tinkuysabor.viewcomponent.ambient
 import android.content.Context
 import android.util.AttributeSet
 
-class LoadingComponentAmbient<VH>(context: Context, attrs: AttributeSet?) :
+open class LoadingComponentAmbient<VH>(context: Context, attrs: AttributeSet?) :
     ViewComponentAmbient<VH>(context = context, attrs = attrs) {
 
 }
