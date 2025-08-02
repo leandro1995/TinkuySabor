a74a4eb0c71eb5a760ce95afc5220b70785c01a8
LEANDRO\leccb <Leandro>
2025-08-02 05:43:30 -0500

    Crear clase de cargando que llevara funciones específicas de solo carga en forma visual

---
+++ app/src/main/java/com/leandro1995/tinkuysabor/viewcomponent/ambient/LoadingComponentAmbient.kt
@@ -0,0 +1,9 @@
+package com.leandro1995.tinkuysabor.viewcomponent.ambient
+
+import android.content.Context
+import android.util.AttributeSet
+
+class LoadingComponentAmbient<VH>(context: Context, attrs: AttributeSet?) :
+    ViewComponentAmbient<VH>(context = context, attrs = attrs) {
+
+}
