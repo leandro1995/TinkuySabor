de6dac845adcdcc708a3b9887b78ced3295c49cd
LEANDRO\leccb <Leandro>
2025-08-16 16:01:10 -0500

    Maquetar parte inicial del detalle de lugar turístico

--- app/src/main/java/com/leandro1995/tinkuysabor/activity/TourismDetailActivity.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/activity/TourismDetailActivity.kt
@@ -10,12 +10,6 @@ import com.leandro1995.tinkuysabor.R
 class TourismDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tourism_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
