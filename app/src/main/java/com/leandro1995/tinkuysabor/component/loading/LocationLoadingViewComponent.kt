7a6e751be4c63a808276b282ba42990fa2671f48
LEANDRO\leccb <Leandro>
2025-08-19 16:37:50 -0500

    Crear componente base para cargando localización

---
+++ app/src/main/java/com/leandro1995/tinkuysabor/component/loading/LocationLoadingViewComponent.kt
@@ -0,0 +1,30 @@
+package com.leandro1995.tinkuysabor.component.loading
+
+import android.content.Context
+import android.util.AttributeSet
+import com.leandro1995.tinkuysabor.R
+import com.leandro1995.tinkuysabor.component.ambient.LoadingComponentAmbient
+import com.leandro1995.tinkuysabor.databinding.ViewComponentLoadingLocationBinding
+
+class LocationLoadingViewComponent(context: Context, attrs: AttributeSet?) :
+    LoadingComponentAmbient<ViewComponentLoadingLocationBinding>(context = context, attrs = attrs) {
+
+    private lateinit var viewComponentLoadingLocationBinding: ViewComponentLoadingLocationBinding
+
+    init {
+        initView(dataBinding = dataBinding(layoutId = R.layout.view_component_loading_location))
+    }
+
+    override fun initView(dataBinding: ViewComponentLoadingLocationBinding) {
+        viewComponentLoadingLocationBinding = dataBinding
+
+    }
+
+    override fun gone() {
+        viewComponentLoadingLocationBinding.loadingLinear.visibility = GONE
+    }
+
+    override fun visible() {
+        viewComponentLoadingLocationBinding.loadingLinear.visibility = VISIBLE
+    }
+}
