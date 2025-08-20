ddbac8e8591783ba7606b8fb008821cf8649daa9
LEANDRO\leccb <Leandro>
2025-08-20 15:46:43 -0500

    Habilitando OnItemSelectedListener para HomeActivity

--- app/src/main/java/com/leandro1995/tinkuysabor/activity/HomeActivity.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/activity/HomeActivity.kt
@@ -1,16 +1,20 @@
 package com.leandro1995.tinkuysabor.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
 import com.leandro1995.tinkuysabor.R
 import com.leandro1995.tinkuysabor.databinding.ActivityHomeBinding
 import com.leandro1995.tinkuysabor.extension.bindingUtil
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     private lateinit var activityHomeBinding: ActivityHomeBinding
+    private var itemSelect = R.id.navigation_home
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +28,14 @@ class HomeActivity : AppCompatActivity() {
         (supportFragmentManager.findFragmentById(R.id.homeContentView) as NavHostFragment).navController.apply {
             activityHomeBinding.homeNavigation.setupWithNavController(this)
         }
+        activityHomeBinding.homeNavigation.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (itemSelect != item.itemId) {
+            itemSelect = item.itemId
+            activityHomeBinding.homeContentView.findNavController().navigate(item.itemId)
+        }
+        return true
     }
 }
