c86d2d086a36adb7d05455d10aac8a040c5d0b2f
LEANDRO\leccb <Leandro>
2025-08-22 04:29:42 -0500

    Colocar toolbar en lista de tuors

--- app/src/main/java/com/leandro1995/tinkuysabor/fragment/TourismListFragment.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/fragment/TourismListFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import com.leandro1995.tinkuysabor.R
 import com.leandro1995.tinkuysabor.databinding.FragmentTourismListBinding
 import com.leandro1995.tinkuysabor.extension.bindingUtil
+import com.leandro1995.tinkuysabor.model.design.Toolbar
 
 class TourismListFragment : Fragment() {
 
@@ -20,6 +21,11 @@ class TourismListFragment : Fragment() {
         fragmentTourismListBinding = bindingUtil(
             layoutId = R.layout.fragment_tourism_list, inflater = inflater, container = container
         )
+
+        Toolbar(
+            materialToolbar = fragmentTourismListBinding.includeAppBar.toolbar,
+            title = getString(R.string.list_tourist_places_text_title)
+        ).create()
 
         return fragmentTourismListBinding.root
     }
