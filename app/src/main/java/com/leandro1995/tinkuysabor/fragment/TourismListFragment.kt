0221c3eb8cf8ca70b3b5c133a72fc7ce0a3ea052
LEANDRO\leccb <Leandro>
2025-08-22 04:25:33 -0500

    Reinicar fragmento de tourismList

--- app/src/main/java/com/leandro1995/tinkuysabor/fragment/TourismListFragment.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/fragment/TourismListFragment.kt
@@ -5,21 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.navGraphViewModels
 import com.leandro1995.tinkuysabor.R
 import com.leandro1995.tinkuysabor.databinding.FragmentTourismListBinding
 import com.leandro1995.tinkuysabor.extension.bindingUtil
-import com.leandro1995.tinkuysabor.extension.viewLifecycleOwner
-import com.leandro1995.tinkuysabor.intent.callback.TourismListIntentCallBack
-import com.leandro1995.tinkuysabor.intent.config.TourismListIntentConfig
-import com.leandro1995.tinkuysabor.model.design.Loading
-import com.leandro1995.tinkuysabor.model.design.Toolbar
-import com.leandro1995.tinkuysabor.model.entity.Tour
-import com.leandro1995.tinkuysabor.viewmodel.TourismListViewModel
 
-class TourismListFragment : Fragment(), TourismListIntentCallBack {
-
-    private val tourismListViewModel by navGraphViewModels<TourismListViewModel>(R.id.home_navigation)
+class TourismListFragment : Fragment() {
 
     private lateinit var fragmentTourismListBinding: FragmentTourismListBinding
 
@@ -31,47 +21,6 @@ class TourismListFragment : Fragment(), TourismListIntentCallBack {
             layoutId = R.layout.fragment_tourism_list, inflater = inflater, container = container
         )
 
-        fragmentTourismListBinding.tourismListViewModel = tourismListViewModel
-
-        viewLifecycleOwner {
-            /*tourismListViewModel.intentActionMutableStateFlow.collect { tourismListIntentAction ->
-                TourismListIntentConfig(
-                    tourismListIntentAction = tourismListIntentAction,
-                    tourismListIntentCallBack = this
-                )
-            }*/
-        }
-
-        tourismListViewModel.action.invoke(TourismListViewModel.INIT_VIEW)
-
         return fragmentTourismListBinding.root
-    }
-
-    override fun tourismArrayList(tourismArrayList: ArrayList<Tour>) {
-        fragmentTourismListBinding.tourismListLoadingRecyclerViewComponent.setAdapter(arrayList = tourismArrayList)
-    }
-
-    override fun showLoading(loading: Loading) {
-        fragmentTourismListBinding.tourismListLoadingRecyclerViewComponent.start(
-            loading = loading,
-            method = { tourismListViewModel.startService(idService = loading.idService) })
-    }
-
-    override fun messageError(idMessageError: Int) {
-        fragmentTourismListBinding.tourismListLoadingRecyclerViewComponent.messageError(
-            messageError = getString(idMessageError), buttonError = {
-                tourismListViewModel.action.invoke(TourismListViewModel.TOURISM_LIST)
-            })
-    }
-
-    override fun initView() {
-        fragmentTourismListBinding.apply {
-            Toolbar(
-                materialToolbar = fragmentTourismListBinding.includeAppBar.toolbar,
-                title = getString(R.string.list_tourist_places_text_title)
-            ).create()
-        }
-
-        tourismListViewModel.action.invoke(TourismListViewModel.TOURISM_LIST)
     }
 }
--- app/src/main/java/com/leandro1995/tinkuysabor/intent/callback/TourismListIntentCallBack.kt
+++
@@ -1,9 +0,0 @@
-package com.leandro1995.tinkuysabor.intent.callback
-
-import com.leandro1995.tinkuysabor.intent.callback.ambient.IntentCallBackAmbient
-import com.leandro1995.tinkuysabor.intent.callback.ambient.ServiceIntentCallBackAmbient
-import com.leandro1995.tinkuysabor.model.entity.Tour
-
-interface TourismListIntentCallBack : ServiceIntentCallBackAmbient, IntentCallBackAmbient {
-    fun tourismArrayList(tourismArrayList: ArrayList<Tour>)
-}
