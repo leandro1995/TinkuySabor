fb991056c20617d40ef739ed59c6224c37bf647b
LEANDRO\leccb <Leandro>
2025-08-22 04:04:36 -0500

    Remover imports sin usar

--- app/src/main/java/com/leandro1995/tinkuysabor/fragment/ProfileFragment.kt
+++ app/src/main/java/com/leandro1995/tinkuysabor/fragment/ProfileFragment.kt
@@ -13,7 +13,6 @@ import com.leandro1995.tinkuysabor.databinding.FragmentProfileBinding
 import com.leandro1995.tinkuysabor.extension.bindingUtil
 import com.leandro1995.tinkuysabor.extension.viewLifecycleOwner
 import com.leandro1995.tinkuysabor.intent.config.event.ProfileIntentEventConfig
-import com.leandro1995.tinkuysabor.intent.event.ProfileIntentEvent
 import com.leandro1995.tinkuysabor.intent.event.config.callback.ProfileIntentEventCallBack
 import com.leandro1995.tinkuysabor.model.design.Toolbar
 import com.leandro1995.tinkuysabor.viewmodel.ProfileViewModel
